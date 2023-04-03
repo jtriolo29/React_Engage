dfdc3be035a830742fc7c5984684ce35a4978166
jtriolo29 <87533646+jtriolo29@users.noreply.github.com>
2023-04-03 19:24:34 -0400

    Add context API exercises

---
+++ 24_CreateConsumeContext_API/ShoppingCartContext/src/context/ShoppingContext.jsx
@@ -0,0 +1,24 @@
+import { createContext, useState } from "react";
+
+export const CartContext = createContext({
+  user: 1,
+  items: [],
+  total: 0,
+  selectItems: function () {},
+});
+
+export function CartContextProvider({ children }) {
+  const [itemsSelected, setItemsSelected] = useState([]);
+  const [cartTotal, setCartTotal] = useState(0);
+
+  let cartValue = {
+    items: itemsSelected,
+    total: parseFloat(cartTotal).toFixed(2),
+    selectItems: setItemsSelected,
+    setTotal: setCartTotal,
+  };
+
+  return (
+    <CartContext.Provider value={cartValue}>{children}</CartContext.Provider>
+  );
+}
